5f627e6180fdaedc75334691dde7c9e971c6b8e4
roryk <rory.kirchner@gmail.com>
2011-11-03 21:45:03 -0400

    Added mockups of the main and game setup screens.

---
+++ Quarkeon Express/GameSetupScreen.h
@@ -0,0 +1,13 @@
+//
+//  GameSetupScreen.h
+//  Quarkeon Express
+//
+//  Created by Rory Kirchner on 11/3/11.
+//  Copyright 2011 MIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameSetupScreen : UIViewController
+
+@end
